23b1ebe9a1387aa07f90905cec938cbfa872ade8
Babichev Igor <himic5@yandex.ru>
2024-10-15 15:11:24 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 
 apt update && apt upgrade -y
-apt install iotop htop ripgrep mc iptraf-ng rlwrap ncdu
+apt install iotop htop ripgrep mc iptraf-ng rlwrap ncdu -y
+apt install build-essential linux-headers-$(uname -r) -y
 
 # включаем автологин под пользователем kali
 sed -i -e "s/#autologin-user=/autologin-user=kali/g" /etc/lightdm/lightdm.conf
@@ -14,6 +15,7 @@ echo "Hidden=true" >> /etc/xdg/autostart/light-locker.desktop
 xset s 0 0 
 
 # install Havoc
+# sudo apt install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev libspdlog-dev python3-dev libboost-all-dev mingw-w64 nasm 
 cd /opt
 #git clone https://github.com/HavocFramework/Havoc.git
 git clone https://github.com/RalfHacker/Havoc.git
